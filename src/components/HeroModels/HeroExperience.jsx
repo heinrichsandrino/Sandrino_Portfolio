f23df1838ac4246a60c729d9aedc6e946b503988
Heinrich C. Sandrino <heinrichsandrino@gmail.com>
2025-06-27 21:56:51 +0800

    hero lights

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,14 +2,13 @@ import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import {useMediaQuery} from "react-responsive";
 import {Room} from "./Room";
+import HeroLights from "./HeroLights.jsx";
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({query: "(max-width: 1024px)"});
     const isMobile = useMediaQuery({query: "(max-width: 728px)"});
     return (
         <Canvas camera={{position: [0, 0, 15], fov: 45}}>
-            <ambientLight intensity={0.2} color="1a1a40"/>
-            <directionalLight position={[5,5,5]} intensity={1}/>
             <OrbitControls
                 enablePan={false}
                 enableZoom={!isTablet}
@@ -18,6 +17,7 @@ const HeroExperience = () => {
                 minPolarAngle={Math.PI/5}
                 maxPolarAngle={Math.PI/2}
             />
+            <HeroLights/>
             <group
             scale = {isMobile ? 0.7 : 1}
             position = {[0, -3.5, 0]}
