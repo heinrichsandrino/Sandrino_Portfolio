d5d9f20ecd2eecc11c973f063fc58129c1309bd4
Heinrich C. Sandrino <heinrichsandrino@gmail.com>
2025-07-04 13:02:15 +0800

    added in Showcase

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {gsap} from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {useGSAP} from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+    useGSAP(()=>{
+        gsap.fromTo(sectionRef.current,
+            {opacity: 0},
+            {opacity: 1, duration: 1.5}
+        )
+    },[]);
     return (
-        <div id = "work" className="app-showcse">
+        <section id = "work" ref = {sectionRef} className="app-showcse">
             <div className="w-full">
                 <div className="showcaselayout">
                     {/*LEFT*/}
-                    <div className = "first-project-wrapper">
+                    <div className = "first-project-wrapper" ref={project1Ref}>
                         <div>
                             <img src = {import.meta.env.BASE_URL + "images/project1.png"} alt= "Ryde"/>
                         </div>
@@ -18,9 +34,25 @@ const ShowcaseSection = () => {
                         </div>
                     </div>
                     {/*RIGHT*/}
+                    <div className = "project-list-wrapper overflow-hidden">
+                        <div className = "project" ref={project2Ref}>
+                            <div className = "image-wrapper bg-[#ffefdb]">
+                                <img src = {import.meta.env.BASE_URL + "images/project2.png"} alt= "Library Management Platform"/>
+                            </div>
+                            <h2>Library Management Platform</h2>
+                        </div>
+
+                        <div className = "project" ref={project3Ref}>
+                            <div className = "image-wrapper bg-[#ffe7db]">
+                                <img src = {import.meta.env.BASE_URL + "images/project3.png"} alt= "YC Directory"/>
+                            </div>
+                            <h2>YC Directory</h2>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 export default ShowcaseSection
