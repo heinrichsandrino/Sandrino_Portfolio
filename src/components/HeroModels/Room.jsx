dcc2d9cca6256485e40ce299a218049043fd25fa
Heinrich C. Sandrino <heinrichsandrino@gmail.com>
2025-07-04 17:06:38 +0800

    fixed the 3d Room orientation

--- src/components/HeroModels/Room.jsx
+++ src/components/HeroModels/Room.jsx
@@ -39,7 +39,7 @@ export function Room(props) {
     });
 
     return (
-        <group {...props} dispose={null}>
+        <group {...props} dispose={null} rotation={[0, Math.PI, 0]}>
             <EffectComposer>
                 <SelectiveBloom
                     selection={screensRef}
