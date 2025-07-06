7d2a6b267b9b4dea0a0dd7a3305090724932e3ba
Heinrich C. Sandrino <heinrichsandrino@gmail.com>
2025-07-06 11:23:25 +0800

    Finished Tech Stack Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.jsx";
 import LogoSection from "./sections/LogoSection.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
 const App = () => {
     return (
  <>
@@ -13,6 +14,7 @@ const App = () => {
      <LogoSection/>
      <FeatureCards/>
      <ExperienceSection/>
+     <TechStack/>
  </>
     )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -113,31 +113,31 @@ const techStackImgs = [
 const techStackIcons = [
     {
         name: "React Developer",
-        modelPath: "/models/react_logo-transformed.glb",
+        modelPath: `${import.meta.env.BASE_URL}models/react_logo-transformed.glb`,
         scale: 1,
         rotation: [0, 0, 0],
     },
     {
         name: "Python Developer",
-        modelPath: "/models/python-transformed.glb",
+        modelPath: `${import.meta.env.BASE_URL}models/python-transformed.glb`,
         scale: 0.8,
         rotation: [0, 0, 0],
     },
     {
         name: "Backend Developer",
-        modelPath: "/models/node-transformed.glb",
+        modelPath: `${import.meta.env.BASE_URL}models/node-transformed.glb`,
         scale: 5,
         rotation: [0, -Math.PI / 2, 0],
     },
     {
         name: "Interactive Developer",
-        modelPath: "/models/three.js-transformed.glb",
+        modelPath: `${import.meta.env.BASE_URL}models/three.js-transformed.glb`, // <- rename to avoid ".js" confusion
         scale: 0.05,
         rotation: [0, 0, 0],
     },
     {
         name: "Project Manager",
-        modelPath: "/models/git-svg-transformed.glb",
+        modelPath: `${import.meta.env.BASE_URL}models/git-svg-transformed.glb`,
         scale: 0.05,
         rotation: [0, -Math.PI / 4, 0],
     },
