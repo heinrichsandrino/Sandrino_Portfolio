b804e192f41f933d6c873e47f14450f243363831
Heinrich C. Sandrino <heinrichsandrino@gmail.com>
2025-07-08 12:13:01 +0800

    fix: base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ✅ Important for GitHub Pages routing
+  base: '/Sandrino_Portfolio/', // ✅ Important for GitHub Pages routing
   plugins: [
     react(),
     tailwindcss(),
